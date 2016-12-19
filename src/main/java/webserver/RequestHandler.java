f15b5a6ad6f5d69375d699e41927e296d15ccf30
hanjaehyeong <hanjaehyeong@192.168.0.8>
2016-12-18 23:35:13 -0500

    index.html 1차 요구사항 개

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,15 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +29,34 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        	String str = br.readLine();
+        	String getReq = "";
+        	while( str != null && !"".equals(str)){
+        		log.debug(str);
+        		
+        		if(str.indexOf("GET") >= 0){
+        			getReq = str;
+        		}
+        		str = br.readLine();        		
+        	}
+        	
+        	String[] get = getReq.split(" ");
+        	byte[] body = null;
+        	if("/index.html".equals(get[1])){
+        		  body = Files.readAllBytes(new File("./webapp"+get[1]).toPath());
+//        	      BufferedReader brFile = new BufferedReader(new FileReader("/webapp/index.html"));
+//        	      str = brFile.readLine();
+//        	      while( str != null && !"".equals(str)){
+//        	    	log.debug(str);
+//        	    	str = brFile.readLine();
+//        	      }
+        	}
+        	
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+        	if(!"/index.html".equals(get[1])){
+        		body = "Hello World".getBytes();
+        	}
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
